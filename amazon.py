57be5a762f75e6e9de4742205cd7a396e0606af2
lupin <tubin.rajan@gmail.com>
2022-10-13 21:08:40 +0530

    final program

--- amazon.py
+++ amazon.py
@@ -1,9 +1,15 @@
 from bs4 import BeautifulSoup
 import requests, pyperclip
-import re
+
 def amazon():
-    URL = pyperclip.paste()
+
+    # getting link by pasting from clipboard
+    link1 = pyperclip.paste()
+    # getting browser url to add your affliate code
+    data = requests.request("GET", link1)
+    m_url = data.url
     
+    URL = m_url
 
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.0.0 Safari/537.36"}
     # headers can be found by searching on your browser 'what is my header'.
@@ -17,6 +23,8 @@ def amazon():
 
     soup2 = BeautifulSoup(soup1.prettify(), "html.parser")
     
+
+    
   
 
     if soup2.find(id='productTitle'): #through the html finding the product tittle class  
@@ -25,24 +33,26 @@ def amazon():
     else:
         pass
     
-    # if '?' in URL: # this is for the link changing from the original link to our desired 
+    if '?' in URL: # this is for the link changing from the original link to our desired 
              
-    #         original = URL.split('?')
-    #         print(original)
-    #         my_link = original[0] + '?tag=dealteck-21'           
-    # else:
-    #     pass  
-   
+            original = URL.split('?')
+            print(original)
+            my_link = original[0] + '?tag=dealteck-21'           
+    else:
+        pass  
+   #class offscreen contains the list value of the price
     if soup2.select(".a-offscreen"):            
         price = soup2.select(".a-offscreen")[0]
         p =(price.text)
         k =((p.split()))        
        
 
-        j = '👉🏻 '+ title + '\n\n✅offer price  '+ str(k[0]) + '\n'
-        # +'\n buy link -- ' + my_link
+        j = '👉🏻 '+ title + '\n\n✅offer price  '+ str(k[0]) + '\n buy link -- ' + my_link
 
         pyperclip.copy(j) 
     else:
         pyperclip.copy("no price found")
 amazon()
+
+
+
